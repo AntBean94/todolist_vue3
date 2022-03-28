c33e3309eea7236a7b0a037793bc7c3d93b5dcbe
gaudi.gwak <kakao_ent@gaudigwakui-MacBookPro.local>
2022-03-29 01:11:04 +0900

    todolist (vue3 examples)

--- src/components/TodoListContainer.vue
+++ src/components/TodoListContainer.vue
@@ -1,32 +1,29 @@
-<template>
-
-</template>
+<template></template>
 <script>
-import { ref, readonly, provide, compile } from 'vue';
+import { ref, readonly, provide, compile } from "vue";
 
 export default {
-    name: 'TodoListContainer',
-    setup() {
-        // todolist 객체와 하위컴포넌트로 전달
-        const todos = ref([])
-        provide('todos', readonly(todos))
-        // todolist 객체 초기화
-        const initTodos = (init_todos) => {
-            todos.value = init_todos
-        }
+  name: "TodoListContainer",
+  setup() {
+    // todolist 객체와 하위컴포넌트로 전달
+    const todos = ref([]);
+    provide("todos", readonly(todos));
+    // todolist 객체 초기화
+    const initTodos = (init_todos) => {
+      todos.value = init_todos;
+    };
 
-        // todolist 조작 함수
-        const addTodo = (todo, date) => {}
-        const removeTodo = (id) => {}
-        const completeTodo = (id) => {}
+    // todolist 조작 함수
+    const addTodo = (todo, date) => {};
+    const removeTodo = (id) => {};
+    const completeTodo = (id) => {};
 
-        // 함수를 provide로 하위 컴포넌트에 전달
-        provide('addTodo', addTodo)
-        provide('removeTodo', removeTodo)
-        provide('completeTodo', completeTodo)
-    }
-}
+    // 함수를 provide로 하위 컴포넌트에 전달
+    provide("addTodo", addTodo);
+    provide("removeTodo", removeTodo);
+    provide("completeTodo", completeTodo);
+  },
+};
 </script>
 <style>
-    
 </style>
--- src/compositions/storage.js
+++ src/compositions/storage.js
@@ -1,24 +1,24 @@
 import { reactive, toRefs } from 'vue';
 
 export const useStorage = () => {
-    const KEY = 'my-todo-list';
-    const storage_obj = reactive({ storage_id: 0 })
-    const loadTodos = (initTodos) => {
-        let temp_todos = JSON.parse(localStorage.getItem(KEY) || '[]')
-        // 배열형태의 데이터에 id값을 부여
-        temp_todos.forEach((todo, idx) => {
-            todo.id = idx
-        })
-        storage_obj.storage_id = temp_todos.length
-        initTodos(temp_todos)
-    }
-    const saveTodos = (todos) => {
-        // 로컬스토리지에 배열형태로 저장
-        localStorage.setItem(KEY, JSON.stringify(todos.value))
-    }
-    return {
-        ...toRefs(storage_obj),     // spread operator로 풀린 반응성을 다시 주입하기 위해 toRefs API 사용
-        loadTodos,
-        saveTodos
-    }
+  const KEY = 'my-todo-list';
+  const storage_obj = reactive({ storage_id: 0 })
+  const loadTodos = (initTodos) => {
+    let temp_todos = JSON.parse(localStorage.getItem(KEY) || '[]')
+    // 배열형태의 데이터에 id값을 부여
+    temp_todos.forEach((todo, idx) => {
+      todo.id = idx
+    })
+    storage_obj.storage_id = temp_todos.length
+    initTodos(temp_todos)
+  }
+  const saveTodos = (todos) => {
+    // 로컬스토리지에 배열형태로 저장
+    localStorage.setItem(KEY, JSON.stringify(todos.value))
+  }
+  return {
+    ...toRefs(storage_obj),     // spread operator로 풀린 반응성을 다시 주입하기 위해 toRefs API 사용
+    loadTodos,
+    saveTodos
+  }
 }
